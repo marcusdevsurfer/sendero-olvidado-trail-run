20e8c8e399869ebf58aa3de5c976353310026499
markdevsurfer <marcus.sea13@gmail.com>
2026-06-17 15:39:41 -0600

    refactor: add image configuration for remote patterns in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
